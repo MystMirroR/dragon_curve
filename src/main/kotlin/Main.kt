686ac246d57372664ff344a0c481327d913a7dc5
--get-all <mystmirror@mail.ru>
2023-11-29 16:35:28 +0300

    Naming

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,16 @@
 fun main(args: Array<String>) {
 
-    var calcR2 = Rectangle(Pair(0, 0), Pair(0, 1), Pair(1, 1), Pair(1, 0))
+    var maxShapeCalculated = Rectangle(Pair(0, 0), Pair(0, 1), Pair(1, 1), Pair(1, 0))
     var pivot = Pair(1L, 1L)
     val startVector = Pair(1L, 1L)
     var endVector: Pair<Long, Long> = startVector
     println("WIDTH :  1   HEIGHT: 1")
     for (i in 1..100) {
-        val shape = calcR2.rotate90degress(pivot)
+        val rotatedShape = maxShapeCalculated.rotate90degress(pivot)
         endVector = plusVector(endVector, rotateVector(endVector))
         pivot = endVector
-        val maxShape = calcR2.maxRectangleOfTwo(shape)
-        calcR2 = maxShape
+        val maxShape = maxShapeCalculated.maxRectangleOfTwo(rotatedShape)
+        maxShapeCalculated = maxShape
         val maxX = abs(maxShape.getAllX().min()) + abs(maxShape.getAllX().max())
         val maxY = abs(maxShape.getAllY().min()) + abs(maxShape.getAllY().max())
         println("WIDTH :  $maxX   HEIGHT: $maxY")
